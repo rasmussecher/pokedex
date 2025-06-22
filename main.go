073ab5b431e90e0d3ea6505dfa1f0c321c4d57e4
Rasmus Secher <rse@embankment.com>
2025-06-22 13:56:52 +0200

    Implement REPL for Pokedex

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		fmt.Println("Your command was:", strings.Fields(text)[0])
+	}
 }
 
 func cleanInput(text string) []string {
